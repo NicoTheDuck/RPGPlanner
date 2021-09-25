2265e3ecc5d995a11d424b96d27b593ad4859059
NicoTheDuck <nicolas.coral1@gmail.com>
2021-09-25 13:07:50 +0200

    Ajout du menu de fermeture

--- src/main/java/gui/controllers/MainWindowController.java
+++ src/main/java/gui/controllers/MainWindowController.java
@@ -2,6 +2,9 @@ package gui.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
@@ -14,5 +17,10 @@ public class MainWindowController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     }    
+
+    @FXML
+    private void closeApplication(ActionEvent event) {
+        Platform.exit();
+    }
     
 }
